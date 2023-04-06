1fc98d8d97541371c0b2623fb43c393f33addd0f
Diego Garavito <44468746+DGarHen@users.noreply.github.com>
2023-04-06 09:37:16 -0500

    basic trainning Primitive vs Reference Type

---
+++ src/main/java/com/trainning/java/Basics.java
@@ -0,0 +1,24 @@
+package com.trainning.java;
+
+import java.util.Arrays;
+
+public class Basics {
+    public static void main(String[] args) {
+        comparePrimitiveVSReferenceType();
+    }
+
+    public static void comparePrimitiveVSReferenceType(){
+        int i = 5;
+        int[] I = {5};
+        changePrimitive(i);
+        changeReference(I);
+        System.out.println("primitive:"+i);
+        System.out.println("reference:"+ Arrays.toString(I));
+    }
+    private static void changePrimitive(int a){
+        a++;
+    }
+    private static void changeReference(int[] a){
+        a[0]++;
+    }
+}
